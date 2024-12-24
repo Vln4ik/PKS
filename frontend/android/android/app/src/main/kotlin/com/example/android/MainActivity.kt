5c1bd80aaad866e3db50d96eb80af28532b0b0b1
Кирюша <126402902+Vln4ik@users.noreply.github.com>
2024-12-23 20:46:38 -0400

    Практическое задание 8 REMAKE

---
+++ frontend/android/android/app/src/main/kotlin/com/example/android/MainActivity.kt
@@ -0,0 +1,9 @@
+<<<<<<<< Updated upstream:frontend/android/android/app/src/main/kotlin/com/example/android/MainActivity.kt
+package com.example.android
+========
+package com.example.front
+>>>>>>>> Stashed changes:frontend/android/app/src/main/kotlin/com/example/pr9/MainActivity.kt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
